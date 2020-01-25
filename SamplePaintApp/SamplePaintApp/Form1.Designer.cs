ed6af5a6626671fd2f7943396a315f05138f7c70
nilayyaren <58950382+nilayyaren@users.noreply.github.com>
2020-01-25 14:18:57 +0300

    Draw Pic

---
+++ SamplePaintApp/SamplePaintApp/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace SamplePaintApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbSize = new System.Windows.Forms.ComboBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.panelSketch = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // cmbSize
+            // 
+            this.cmbSize.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cmbSize.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.cmbSize.Font = new System.Drawing.Font("Rockwell", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbSize.Items.AddRange(new object[] {
+            "10",
+            "15",
+            "20",
+            "25",
+            "30",
+            "35"});
+            this.cmbSize.Location = new System.Drawing.Point(12, 21);
+            this.cmbSize.Name = "cmbSize";
+            this.cmbSize.Size = new System.Drawing.Size(80, 34);
+            this.cmbSize.TabIndex = 0;
+            this.cmbSize.Text = "Size";
+            this.cmbSize.SelectedIndexChanged += new System.EventHandler(this.cmbSize_SelectedIndexChanged);
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.FullOpen = true;
+            this.colorDialog1.SolidColorOnly = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnClear.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClear.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnClear.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(1311, 19);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(143, 39);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear All";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnColor
+            // 
+            this.btnColor.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnColor.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnColor.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnColor.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnColor.Location = new System.Drawing.Point(98, 20);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(151, 39);
+            this.btnColor.TabIndex = 2;
+            this.btnColor.Text = "Color Box";
+            this.btnColor.UseVisualStyleBackColor = false;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // panelSketch
+            // 
+            this.panelSketch.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panelSketch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelSketch.Location = new System.Drawing.Point(12, 68);
+            this.panelSketch.Name = "panelSketch";
+            this.panelSketch.Size = new System.Drawing.Size(1442, 630);
+            this.panelSketch.TabIndex = 3;
+            this.panelSketch.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelSketch_MouseDown);
+            this.panelSketch.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelSketch_MouseMove);
+            this.panelSketch.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelSketch_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(1466, 710);
+            this.Controls.Add(this.panelSketch);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.cmbSize);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbSize;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.Panel panelSketch;
+    }
+}
+
---
+++ SamplePaintApp/SamplePaintApp/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SamplePaintApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ColorDialog clrBox = new ColorDialog();
+        int size = 10;
+        bool drawPic;
+        int startX,startY;
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            clrBox.ShowDialog();
+        }
+
+        private void cmbSize_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            size = Convert.ToInt32(cmbSize.SelectedItem);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            panelSketch.Invalidate();
+        }
+
+        private void panelSketch_MouseDown(object sender, MouseEventArgs e)
+        {
+            drawPic = true;
+            startX= e.X;
+            startY=e.Y;
+        }
+
+        private void panelSketch_MouseUp(object sender, MouseEventArgs e)
+        {
+            drawPic = false;
+        }
+
+        private void panelSketch_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left && drawPic)
+            {
+                Graphics graph = panelSketch.CreateGraphics();
+                Pen pen = new Pen(clrBox.Color, size);
+                Point point1 = new Point(startX, startY);
+                Point point2 = new Point(e.X, e.Y);
+                graph.DrawLine(pen, point1, point2);
+                startX = e.X;
+                startY = e.Y;
+            }
+            else if(e.Button==MouseButtons.Right && drawPic)
+            {
+                Graphics graph = panelSketch.CreateGraphics();
+                Pen pen = new Pen(panelSketch.BackColor, size);
+                Point point1 = new Point(startX, startY);
+                Point point2 = new Point(e.X, e.Y);
+                graph.DrawLine(pen, point1, point2);
+                startX = e.X;
+                startY = e.Y;
+            }
+        }
+    }
+}
